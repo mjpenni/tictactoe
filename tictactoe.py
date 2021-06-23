c0b52b9712c6d07e73c8fa6c19b287d7a47434dc
Marco Penniman <marco.penniman@gmail.com>
2021-06-23 14:45:11 -0700

    pre-pended all variable names with vars to point to the tictactoe_vars.py variables.

--- tictactoe.py
+++ tictactoe.py
@@ -3,29 +3,29 @@ def var_def():
     pass
 
 def print_board():
-    print(board_key[0]+"\r\n"+board_key[1]+"\r\n"+board_key[2]+"\r\n"+
-          board_key[3]+"\r\n"+board_key[4]+"\r\n")
-    #print(boardB)
-    print(board[0])
-    print(board[1])
-    # print(boardB)
-    #print(boardD)
-    #print(boardB)
-    print(board[2])
-    #print(boardB)
-    #print(boardD)
-    #print(boardB)
-    print(board[3])
-    #print(boardB)
+    print(vars.board_key[0]+"\r\n"+vars.board_key[1]+"\r\n"+vars.board_key[2]+"\r\n"+
+          vars.board_key[3]+"\r\n"+vars.board_key[4]+"\r\n")
+    #print(vars.boardB)
+    print(vars.board[0])
+    print(vars.board[1])
+    # print(vars.boardB)
+    #print(vars.boardD)
+    #print(vars.boardB)
+    print(vars.board[2])
+    #print(vars.boardB)
+    #print(vars.boardD)
+    #print(vars.boardB)
+    print(vars.board[3])
+    #print(vars.boardB)
 
 def select_box(player):
-    box = input("In what box number do you want to put an "+player+"? ")
-    row_column = box_pos[str(box)]
-    print(row_column) # debug
-    r0=row_column[0]
-    r1=row_column[1]
-    #board[row_column[0]][row_column[1]]=player
-    board[r0,r1]=player
+    vars.box = input("In what box number do you want to put an "+player+"? ")
+    vars.row_column = box_pos[str(vars.box)]
+    print(vars.row_column) # debug
+    vars.r0=vars.row_column[0]
+    vars.r1=vars.row_column[1]
+    #vars.board[vars.row_column[0]][vars.row_column[1]]=player
+    vars.board[vars.r0,vars.r1]=player
 
 def win_rows_build():
     # verticals: row_147, row_258, row_369
@@ -34,37 +34,37 @@ def win_rows_build():
 
     # for each verticle, horizontal, diagonal:
     # build horizontal rows beginning with 1, 4, 7
-    row_123=row_456=row_789=0
-    row_123+=board[1][1]+board[1][5]+board[1][9]
-    row_456+=board[2][1]+board[2][5]+board[2][9]
-    row_789+=board[3][1]+board[3][5]+board[3][9]
+    vars.row_123=vars.row_456=vars.row_789=0
+    vars.row_123+=vars.board[1][1]+vars.board[1][5]+vars.board[1][9]
+    vars.row_456+=vars.board[2][1]+vars.board[2][5]+vars.board[2][9]
+    vars.row_789+=vars.board[3][1]+vars.board[3][5]+vars.board[3][9]
 
     # build vertical row begtinning with 1, 2, 3
-    row_147=row_258=row_369=0
-    row_147+=board[1][1]+board[2][1]+board[3][1]
-    row_258+=board[1][5]+board[2][5]+board[3][5]
-    row_369+=board[1][9]+board[2][9]+board[3][9]
+    vars.row_147=vars.row_258=vars.row_369=0
+    vars.row_147+=vars.board[1][1]+vars.board[2][1]+vars.board[3][1]
+    vars.row_258+=vars.board[1][5]+vars.board[2][5]+vars.board[3][5]
+    vars.row_369+=vars.board[1][9]+vars.board[2][9]+vars.board[3][9]
 
     # build diagonal rows biginning with 1, 3
-    row_159=row_357=0
-    row_159+=board[1][1]+board[2][5]+board[3][9]
-    row_357+=board[1][9]+board[2][5]+board[3][1]
+    vars.row_159=vars.row_357=0
+    vars.row_159+=vars.board[1][1]+vars.board[2][5]+vars.board[3][9]
+    vars.row_357+=vars.board[1][9]+vars.board[2][5]+vars.board[3][1]
 
     # put rows in an list so they can be interated easily
-    win_rows=[row_123, row_456, row_789, row_147, row_258, row_369, row_159, row_357]
+    vars.win_rows=[vars.row_123, vars.row_456, vars.row_789, vars.row_147, vars.row_258, vars.row_369, vars.row_159, vars.row_357]
 
     def win_test():
         # test X's or O's for winning configuration
             for row in win_rows:
                 if row == win_x:
                     print('X wins')
-                    restart_flag="Y"
+                    vars.restart_flag="Y"
                 elif row == win_o:
                     print('O wins')
-                    restart_flag="Y"
+                    vars.restart_flag="Y"
                 else:
                     print('Keep playing. No winner yet')
-                    restart_flag="N"
+                    vars.restart_flag="N"
 
 def char_pos(square):
     # lookup square (key) & return char_pos & row
