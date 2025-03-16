ca63c2ad3ea8d096eb8c04c9f10f681908dc7349
Mark Penniman <mjpenni@gmail.com>
2025-03-15 18:51:01 -0700

    new file: clearly and simply test for a win

---
+++ victory_test.py
@@ -0,0 +1,9 @@
+#victory_test.py
+
+#check horizontal rows: 123, 456, 789
+#check vertical   rows: 147, 258, 369
+#check diagonal   rows: 159, 357
+
+#to start: win=false
+#each row is checked for 3 matching (X or O) entries
+#1 of 8 successful tests mean victory: win=true
