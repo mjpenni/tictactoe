2db8ba523a9a8e8356d070c8c21979a766a8f6a0
Mark Penniman <mjpenni@gmail.com>
2025-03-19 18:03:21 -0700

    Add victory_test.py test

--- victory_test.py
+++ victory_test.py
@@ -7,3 +7,35 @@
 #to start: win=false
 #each row is checked for 3 matching (X or O) entries
 #1 of 8 successful tests mean victory: win=true
+Xwin=false
+   # horizontal
+if (rows_123[1] and rows_123[2] and rows_123[3] == "X") or
+   (rows_456[1] and rows_456[2] and rows_456[3] == "X") or
+   (rows_789[1] and rows_789[2] and rows_789[3] == "X") or
+   # vertical
+   (rows_147[1] and rows_147[2] and rows_147[3] == "X") or
+   (rows_258[1] and rows_258[2] and rows_258[3] == "X") or
+   (rows_369[1] and rows_369[2] and rows_369[3] == "X") or
+   # diagonal
+   (rows_159[1] and rows_159[2] and rows_159[3] == "X") or
+   (rows_357[1] and rows_357[2] and rows_357[3] == "X"):
+   Xwin=true
+
+Ywin=false
+   # horizontal
+if (rows_123[1] and rows_123[2] and rows_123[3] == "Y") or
+   (rows_456[1] and rows_456[2] and rows_456[3] == "Y") or
+   (rows_789[1] and rows_789[2] and rows_789[3] == "Y") or
+   # vertical
+   (rows_147[1] and rows_147[2] and rows_147[3] == "Y") or
+   (rows_258[1] and rows_258[2] and rows_258[3] == "Y") or
+   (rows_369[1] and rows_369[2] and rows_369[3] == "Y") or
+   # diagonal
+   (rows_159[1] and rows_159[2] and rows_159[3] == "Y") or
+   (rows_357[1] and rows_357[2] and rows_357[3] == "Y"):
+   Ywin=true
+
+if Xwin:
+   return(parameter="X")
+if Ywin:
+   return(parameter="O")
