b07663f39ef144b305954d400e9a9d407dc17e42
Mark Penniman <mjpenni@gmail.com>
2025-03-21 21:00:31 -0700

    testing corrections

--- victory_test.py
+++ victory_test.py
@@ -9,33 +9,50 @@
 #1 of 8 successful tests mean victory: win=true
 Xwin=false
    # horizontal
-if (row_123[1] == "X" and row_123[2] == "X" and row_123[3] == "X") or 
-   (row_456[1] == "X" and row_456[2] == "X" and row_456[3] == "X") or 
-   (row_789[1] == "X" and row_789[2] == "X" and row_789[3] == "X") or 
-   # vertical
-   (row_147[1] == "X" and row_147[2] == "X" and row_147[3] == "X") or 
-   (row_258[1] == "X" and row_258[2] == "X" and row_258[3] == "X") or 
-   (row_369[1] == "X" and row_369[2] == "X" and row_369[3] == "X") or 
-   # diagonal
-   (row_159[1] == "X" and row_159[2] == "X" and row_159[3] == "X") or 
-   (row_357[1] == "X" and row_357[2] == "X" and row_357[3] == "X"):
+if (row_123[1] == "X" and row_123[2] == "X" and row_123[3] == "X"): 
    Xwin = true
-
+elif (row_456[1] == "X" and row_456[2] == "X" and row_456[3] == "X"): 
+   Xwin = true
+elif (row_789[1] == "X" and row_789[2] == "X" and row_789[3] == "X"): 
+   Xwin = true
+   # vertical
+elif (row_147[1] == "X" and row_147[2] == "X" and row_147[3] == "X"): 
+   Xwin = true
+elif (row_258[1] == "X" and row_258[2] == "X" and row_258[3] == "X"): 
+   Xwin = true
+elif (row_369[1] == "X" and row_369[2] == "X" and row_369[3] == "X"): 
+   Xwin = true
+   # diagonal
+elif (row_159[1] == "X" and row_159[2] == "X" and row_159[3] == "X"): 
+   Xwin = true
+elif (row_357[1] == "X" and row_357[2] == "X" and row_357[3] == "X"):
+   Xwin = true
+else:
+   Xwin = false
 Ywin=false
    # horizontal
-if (row_123[1] and row_123[2] and row_123[3] == "Y") or
-   (row_456[1] and row_456[2] and row_456[3] == "Y") or
-   (row_789[1] and row_789[2] and row_789[3] == "Y") or
+if (row_123[1] == "Y" and row_123[2] == "Y" and row_123[3] == "Y"): 
+   Ywin = true
+elif (row_456[1] == "Y" and row_456[2] == "Y" and row_456[3] == "Y"): 
+   Ywin = true
+elif (row_789[1] == "Y" and row_789[2] == "Y" and row_789[3] == "Y"): 
+   Ywin = true
    # vertical
-   (row_147[1] and row_147[2] and row_147[3] == "Y") or
-   (row_258[1] and row_258[2] and row_258[3] == "Y") or
-   (row_369[1] and row_369[2] and row_369[3] == "Y") or
+elif (row_147[1] == "Y" and row_147[2] == "Y" and row_147[3] == "Y"): 
+   Ywin = true
+elif (row_258[1] == "Y" and row_258[2] == "Y" and row_258[3] == "Y"): 
+   Ywin = true
+elif (row_369[1] == "Y" and row_369[2] == "Y" and row_369[3] == "Y"): 
+   Ywin = true
    # diagonal
-   (row_159[1] and row_159[2] and row_159[3] == "Y") or
-   (row_357[1] and row_357[2] and row_357[3] == "Y"):
-   Ywin=true
+elif (row_159[1] == "Y" and row_159[2] == "Y" and row_159[3] == "Y"): 
+   Ywin = true
+elif (row_357[1] == "Y" and row_357[2] == "Y" and row_357[3] == "Y"):
+   Ywin = true
+else:
+   Ywin = false
 
 if Xwin:
-   return(parameter="X")
+   return "X"
 if Ywin:
-   return(parameter="O")
+   return "O"
