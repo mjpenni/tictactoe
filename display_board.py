157786198717e02b75ff28d555253e7267c61457
Mark Penniman <mjpenni@gmail.com>
2025-03-23 23:44:11 -0700

    flesh out display_board.py

--- display_board.py
+++ display_board.py
@@ -1,2 +1,7 @@
-# display the game board. Empty or full.
-
+def display_board():
+    print (board_key[0])
+    print (board_key[1])
+    print (board_key[2])
+    print (board_key[3])
+    print (board_key[4])
+    
