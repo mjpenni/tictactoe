181f2cda46196f5f183ab62f0993ab6c065192f0
Mark Penniman <mjpenni@gmail.com>
2025-03-26 00:24:37 -0700

    working changes

--- tic_test.py
+++ tic_test.py
@@ -7,15 +7,15 @@ def char_pos(square):
 #-------------------------------------------------------------------------------------
     
 #main
-# import sim_print eh?
 from build_board import build_board
 from display_board import display_board
-#import game_vars
 from game_vars import select_box
 from game_vars import game_vars
-
-# from game_board.py import win_rows_build (defunct
 from victory_test import victory_test
+
+#dictionary
+grid = {"cell[1]": "!","cell[2]": "!","cell[3]": "!","cell[4]": "!","cell[5]": "!","cell[6]": "!","cell[7]": "!","cell[8]": "!", "cell[9]": "!"}
+
 go="S"
 while go not in "YNQ":
     go = input("Are you ready to play?  Yy, Nn or Qq: ")
@@ -33,18 +33,25 @@ while go not in "YNQ":
             else:
                 print("Please enter X or O.")
         print("player 1 will go first")
+        # for each verticle, horizontal, diagonal:
+        # build horizontal rows beginning with 1, 4, 7
+        row_123=row_456=row_789=["!","!","!","!"]
+        # build vertical row begtinning with 1, 2, 3
+        row_147=row_258=row_369=["!","!","!","!"]
+        # build diagonal rows biginning with 1, 3
+        row_159=row_357=["!","!","!","!"]
         grid = build_board()
         turns=0
         winner="!"
         while winner == "!":
             print ("game start \n\n")
             display_board(grid)
-            game_vars.game_vars()
+            #game_vars()
             turns += 1
             # player 1 make selection & after 3 check for win
-            game_vars.select_box(player1)
+            select_box(player1)
             # player 2 make selection & after 3 check for win
-            game_vars.select_box(player2)
+            select_box(player2)
             if turns >= 3:
                 #check for a win
                 winner = victory_test.victory_test()
